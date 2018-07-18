de2af178be877eb84ec49d744d6fc10994728960
iuvfag <uh22.love.acm@gmail.com>
2018-07-18 17:56:23 +0900

    7/18

--- TestSampleWeb/src/com/internousdev/testsampleweb/util/InputChecker.java
+++ TestSampleWeb/src/com/internousdev/testsampleweb/util/InputChecker.java
@@ -8,9 +8,10 @@ import org.apache.commons.lang3.StringUtils;
 public class InputChecker {
 
 	/**
-	 * このプログラムは渡された値を正規表現化検証するもの
+	 * このプログラムは渡された値を正規表現か検証するもの
 	 * doCheckに9つの引数を渡し、後半部分に当てはまる形式にtrueを入れる
-	 * emailを判別したいのであれば英字、数字、記号を含むため以下のようになる
+	 * emailを判別したいのであれば英字、数字、記号を含むため下記のようになる
+	 * 例(質問の内容、値、最小文字数、最大文字数、true, false, false, true, true)となる
 	 * 結果はList形式で渡されることに注意
 	 */
 
@@ -18,6 +19,7 @@ public class InputChecker {
 
 		//検証した結果を入れるList
 		List<String> stringList = new ArrayList<String>();
+		//画面に表示したいメッセージを代入するList型変数
 		List<String> characterTypeList = new ArrayList<String>();
 
 		//入力欄が空かどうかを検証する
@@ -35,6 +37,10 @@ public class InputChecker {
 		if(value.length() < minLength || value.length() > maxLength){
 			stringList.add(propertyName + "は" + minLength + "文字以上" + maxLength + "文字以下で入力して下さい。");
 		}
+		/**
+		 * 渡されたvalueが最小文字数以下、あるいは最大文字数以上の場合は
+		 * stringListにメッセージを追加
+		 */
 
 		//////////入力された文字のタイプから何を判別するか決める//////////
 		String regularExpression = "";
@@ -43,7 +49,7 @@ public class InputChecker {
 		if(availableAlphabeticCharacters || availableKanji || availableHiragana || availableHalfWidthDigit || availableHalfWidthSymbols || availableKatakana || availableFullWidthSymbols){
 			regularExpression = "[^";
 		}
-		if(!(availableAlphabeticCharacters) || !(availableKanji) || !(availableHiragana) ||!(availableHalfWidthDigit) || !(availableHalfWidthSymbols) || !(availableKatakana) || !(availableFullWidthSymbols)){
+		if(!(availableAlphabeticCharacters) || !(availableKanji) || !(availableHiragana) || !(availableHalfWidthDigit) || !(availableHalfWidthSymbols) || !(availableKatakana) || !(availableFullWidthSymbols)){
 			errorExpression = "[^";
 		}
 
@@ -55,10 +61,88 @@ public class InputChecker {
 		}
 
 		if(availableKanji){
-			regularExpression += "";
+			regularExpression += "一-龯";
+			characterTypeList.add("漢字");
+		}else{
+			errorExpression += "一-龯";
 		}
 
+		if(availableHiragana){
+			regularExpression += "ぁ-ん";
+			characterTypeList.add("ひらがな");
+		}else{
+			errorExpression += "ぁ-ん";
+		}
 
+		if(availableHalfWidthDigit){
+			regularExpression += "0-9";
+			characterTypeList.add("半角数字");
+		}else{
+			errorExpression += "0-9";
+		}
+
+		if(availableHalfWidthSymbols){
+			regularExpression += "@.,;:!#$%&'*+-/=?^_`{|}~";
+			characterTypeList.add("半角記号");
+		}else{
+			errorExpression += "@.,;:!#$%&'*+-/=?^_`{|}~";
+		}
+
+		if(availableKatakana){
+			regularExpression += "ァ-ヺ";
+			characterTypeList.add("カタカナ");
+		}else{
+			errorExpression += "ァ-ヺ";
+		}
+
+		if(availableFullWidthSymbols){
+			regularExpression += "＠．，；：！＃＄％＆’＊＋―／＝？＾＿｀｛｜｝～";
+			characterTypeList.add("全角記号");
+		}else{
+			errorExpression += "＠．，；：！＃＄％＆’＊＋―／＝？＾＿｀｛｜｝～";
+		}
+
+		if(!StringUtils.isEmpty(regularExpression)){
+			regularExpression += "]+";
+		}
+
+		if(!StringUtils.isEmpty(errorExpression)){
+			errorExpression += "]+";
+		}
+		///////////////////////ここまで/////////////////////
+
+
+		//判別した結果に応じてエラーメッセージを返す
+		String characterType = "";
+		for(int i=0; i<characterTypeList.size(); i++){
+			if(i == 0){
+				characterType += characterTypeList.get(i).toString();
+			}else{
+				characterType += "、" + characterTypeList.get(i).toString();
+			}
+		}
+
+		if(errorExpression.equals("")){
+			if(value.matches(regularExpression)){
+				stringList.add(propertyName + "は" + characterType + "で入力して下さい。");
+			}
+		}else{
+			if(value.matches(regularExpression) || (!value.matches(errorExpression) && !value.equals(""))){
+				stringList.add(propertyName + "は" + characterType + "で入力してください。");
+			}
+		}
+
+		return stringList;
+
+
+	}
+
+	public List<String> doPasswordCheck(String password, String reConfirmationPassword){
+		List<String> stringList = new ArrayList<String>();
+		if(!(password.equals(reConfirmationPassword))){
+			stringList.add("入力されたパスワードが異なります。");
+		}
+		return stringList;
 	}
 
 }
